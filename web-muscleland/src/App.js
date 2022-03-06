ca0f74b69a113c4f08f57b2b655374e53ab3f2e0
Kittinan Samakpong <joker.use.3@gmail.com>
2022-03-06 14:07:29 +0700

    set up react&bootstrap

---
+++ web-muscleland/src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNavbar from "./Components/Header/MyNavbar";
+
+function App() {
+  return (
+    <div className="App">
+      <MyNavbar />
+    </div>
+  );
+}
+
+export default App;
